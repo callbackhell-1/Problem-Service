1b4e5252ecbb3dba1c3fb67f0594e06b3e2c1304
Adarsh <jha01adarsh@gmail.com>
2025-06-10 19:06:46 +0530

    Implement problem controller and routing structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/server.config');
+const apiRouter = require('./routes/index');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+
+//routes
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   res.status(200).json({
