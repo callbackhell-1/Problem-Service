4119dc1ca3c3e3cdc0ec5de4fb6e42c26c4e3649
Adarsh <jha01adarsh@gmail.com>
2025-06-10 18:22:55 +0530

    Added basic express server with dotenv and nodemon

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/server.config');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+app.get('/ping', (req, res) => {
+  res.status(200).json({
+    message: 'Pong',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
